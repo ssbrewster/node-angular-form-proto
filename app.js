50db48e94f3469c6c9aa58c5ede0ec4c2fd7e6f8
ssbrewster <ssbrewster@gmail.com>
2015-04-20 13:31:11 +0100

    Database and first pass of REST endpoints
    Hooking up database using Mongoskin
    First pass of REST endpoint for enquiry list

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/node-form-proto', {native_parser:true});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,9 +20,12 @@ app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,28 @@
 var express = require('express');
-var router = express.Router(); 
-var expressValidator = require('express-validator');
+var router = express.Router();
 var xssFilters = require('xss-filters');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Contact the Council' });
+  res.json('index', { title: 'Contact the Council' });
 });
 
 /* GET Enquiry list page. */
 router.get('/enquirylist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('formcollection');
-    collection.find({},{},function(e, docs) {
-        res.render('enquirylist', {
-            "enquirylist" : docs
-        });
+    var db = req.db;    
+    db.collection('formcollection').find().toArray(function (err, items) {
+      if (!err) {  
+        res.json(items);
+      }
+      else {
+        console.log(err);
+      }
     });
 });
 
 /* GET New Enquiry page. */
 router.get('/newenquiry', function(req, res) {
-    res.render('newenquiry', { title: 'Send us an enquiry' });
+    res.json('newenquiry', { title: 'Send us an enquiry' });
 });
 
 /* POST to Add Enquiry Service */ 
@@ -36,20 +37,20 @@ router.post('/addenquiry', function(req, res) {
     var enquiry = xssFilters.inHTMLData(req.body.enquiry);
 
     // Validate the form data
-    req.assert('name', 'Name is required').notEmpty();
-    req.assert('email', 'A valid email address is required').isEmail();
-    req.assert('enquiry', 'Please tell us what your enquiry is').notEmpty();
+    //req.assert('name', 'Name is required').notEmpty();
+    //req.assert('email', 'A valid email address is required').isEmail();
+    //req.assert('enquiry', 'Please tell us what your enquiry is').notEmpty();
 
     var errors = req.validationErrors();
     if (!errors) {
-        res.render('/addenquiry', {
+        res.json('/addenquiry', {
           title: 'Send an enquiry',
           message: 'Your enquiry has been sent successfully',
           errors: {}
        });
     }
     else {
-        res.render('/addenquiry', {
+        res.json('/addenquiry', {
           title: 'Send an enquiry',
           message: '',
           errors: errors
